d5bb48145a61418b4041043d63f1ee240dc625b3
Jamey Kim <jameykim112@gmail.com>
2019-02-04 23:39:06 +1100

    Update dan_murphy_scrape.py

--- dan_murphy_scrape.py
+++ dan_murphy_scrape.py
@@ -1,29 +1,38 @@
 # Import required packages
 from selenium import webdriver #Initialise web browser
-from selenium.webdriver.common.by import By #Search for things
-from selenium.webdriver.support.ui import WebDriverWait #Wait for page to load
-from selenium.webdriver.support import expected_conditions as EC #What to look for on page
-from selenium.common.exceptions import TimeoutException #Handling timeout situations
+import datetime #Print current date
+import pandas as pd
 
 # Create an Incognito instance of Chrome
 option = webdriver.ChromeOptions()
 option.add_argument(" — incognito")
 
 # Find the ChromeDriver and get website
+#TODO: Make URL an input to make solution more flexible
 browser = webdriver.Chrome(executable_path="/Users/janetyuen/Documents/2. Side projects/Web Scraping/chromedriver",chrome_options=option)
 browser.get("https://www.danmurphys.com.au/product/DM_38905/hennessy-vs-cognac-700ml")
-# Note: Include a pause/wait to ensure entire page has loaded before scraping
-
 
 # Find price, brand, container using xPath
 #TODO: Can you include multiple find_elements_by_xpath in one line rather than repeating?
-prices = browser.find_elements_by_xpath('//*[@id="main-content"]/div/shop-product-detail/div/section/div[1]/aside/shop-add-to-cart/div/div[1]/div/div/p[1]/span')
-product_brand = browser.find_elements_by_xpath('//*[@id="main-content"]/div/shop-product-detail/div/section/h1/span[1]')
-product_name = browser.find_elements_by_xpath('//*[@id="main-content"]/div/shop-product-detail/div/section/h1/span[2]')
+prices = browser.find_element_by_xpath('//*[@id="main-content"]/div/shop-product-detail/div/section/div[1]/aside/shop-add-to-cart/div/div[1]/div/div/p[1]/span')
+product_brand = browser.find_element_by_xpath('//*[@id="main-content"]/div/shop-product-detail/div/section/h1/span[1]')
+product_name = browser.find_element_by_xpath('//*[@id="main-content"]/div/shop-product-detail/div/section/h1/span[2]')
 
-#TODO: Format results to be included in Excel Workbook
-prices[0].text
-product_brand[0].text
-product_name[0].text
+# Format results to write to text file
+now = datetime.datetime.now()
+now.strftime("%d-%m-%y")
 
-print("Hello World")
+# Create a Pandas DataFrame 
+prices_df = []
+prices_df.append([now.strftime("%d-%m-%y"),prices.text, product_brand.text, product_name.text])
+data = prices_df[-1][0]+'   '+prices_df[-1][1]
+
+# Import results to existing text file
+outputFile = open("/Users/janetyuen/Documents/2. Side projects/Web Scraping/webscraping_results.txt","a")
+outputFile.write(data)
+outputFile.write("\n")
+outputFile.close()
+
+# Close ChromeDriver
+browser.close()
+
